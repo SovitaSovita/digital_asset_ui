b00ca6ac8bf3d238aceb0c9be4ee7e2ab0fab1cf
simsokhen70 <115085265+simsokhen70@users.noreply.github.com>
2023-12-28 22:21:30 +0700

    sokhen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,37 @@
+import {nextui} from "@nextui-org/react";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
+      height: {
+        '128': '41.8rem',
+        '228': '82%'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary': '#0b0c10',
+        'secondary': '#1f2833',
+        'tertiary': '#c5c6c7',
+        'quaternary': '#66fcf1',
+        'quinary': '#45a29e',
+        'senary': '#5bc0be',
+        'septenary': '#1f2833',
+        'octonary': '#0b0c10',
+        'nonary': '#66fcf1',
+        'denary': '#c5c6c7',
+      },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
